41fdc4cd81400808e329bcb5c89b8e014e75f2ed
DJ-MrJay <djmrjay@gmail.com>
2025-07-14 22:11:04 +0300

    Add Webpack support for the @ alias

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+import path from "path";
 import type { NextConfig } from "next";
+import type { Configuration as WebpackConfig } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config: WebpackConfig) {
+    config.resolve = {
+      ...(config.resolve || {}),
+      alias: {
+        ...(config.resolve?.alias || {}),
+        "@": path.resolve(__dirname, "src"),
+      },
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
